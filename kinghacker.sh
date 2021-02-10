f416b63179aab71a19d717c7b35fc66a1751bb6c
kinghacker2004 <78868434+kinghacker2004@users.noreply.github.com>
2021-02-10 22:33:46 +0530

    Add files via upload

---
+++ kinghacker.sh
@@ -0,0 +1,10 @@
+echo "kinghacker"
+git clone https://github.com/Bhaviktutorials/T-Remix
+echo "T-Remix installed"
+git clone https://github.com/TheSpeedX/TBomb
+echo "TBomb install"
+git clone https://github.com/xHak9x/SocialPhish
+echo "SocialPhish installed"
+git clone https://github.com/vanhienfs/saycheese
+cowsay "installed sucessfully"
+
